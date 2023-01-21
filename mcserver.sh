60d65866d92b069e4634eff15b05e19a323ffd02
okazakee <cristiandcbusiness@gmail.com>
2023-01-21 22:07:24 +0100

    fixed final url check

--- mcserver.sh
+++ mcserver.sh
@@ -16,9 +16,9 @@ fi
 LAZYMC_URL="https://github.com/timvisee/lazymc/releases/download/$LAZYMC_VERSION/lazymc-$LAZYMC_VERSION-linux-$CPU_ARCHITECTURE"
 status_code=$(curl -s -o /dev/null -w '%{http_code}' ${LAZYMC_URL})
 if [ "$status_code" -ne 200 ]
-  then
-    echo "Error: Lazymc version does not exist or is not available. Exiting..."
-    exit 1
+then
+  echo "Error: Lazymc version does not exist or is not available. Exiting..."
+  exit 1
 fi
 wget -O lazymc ${LAZYMC_URL}
 chmod +x lazymc
@@ -41,7 +41,8 @@ case "$SERVER_PROVIDER" in
         then
             # Get the latest MC version
             MC_VERSION=$(wget -qO - $URL | jq -r '.versions[-1]') # "-r" is needed because the output has quotes otherwise
-            if [ $? -ne 0 ]; then
+            if [ $? -ne 0 ];
+            then
               echo "Error: Could not get latest version of Minecraft"
               exit 1
             fi
@@ -51,7 +52,8 @@ case "$SERVER_PROVIDER" in
         then
             # Get the latest build
             SERVER_BUILD=$(wget -qO - $URL | jq '.builds[-1]')
-            if [ $? -ne 0 ]; then
+            if [ $? -ne 0 ];
+            then
               echo "Error: Could not get latest build of $SERVER_PROVIDER"
               exit 1
             fi
@@ -65,7 +67,8 @@ case "$SERVER_PROVIDER" in
         then
           # Get the latest MC version
           MC_VERSION=$(wget -qO - $URL | jq -r '.versions[-1]')
-          if [ $? -ne 0 ]; then
+          if [ $? -ne 0 ];
+          then
             echo "Error: Could not get latest version of Minecraft"
             exit 1
           fi
@@ -75,7 +78,8 @@ case "$SERVER_PROVIDER" in
         then
             # Get the latest build
             SERVER_BUILD=$(wget -qO - $BUILD_URL | jq -r '.builds.latest')
-            if [ $? -ne 0 ]; then
+            if [ $? -ne 0 ];
+            then
               echo "Error: Could not get latest build of $SERVER_PROVIDER"
               exit 1
             fi
@@ -89,21 +93,17 @@ case "$SERVER_PROVIDER" in
         ;;
 esac
 
-#check if build exists
-status_code=$(curl -s -o /dev/null -w '%{http_code}' ${URL})
-if [ "$status_code" -ne 200 ]
-  then
-  echo "Error: Build does not exist or is not available. Exiting..."
-  exit 1
-fi
-
 # Update if necessary
 if [ ! -e ${JAR_NAME} ]
 then
   # Remove old server jar(s)
   rm -f *.jar
   # Download new server jar
-  wget ${URL} -O ${JAR_NAME}
+  if ! curl -f -o ${JAR_NAME} ${URL}
+  then
+      echo "Error: Build does not exist or is not available. Exiting..."
+      exit 1
+  fi
 
   # If this is the first run, accept the EULA
   if [ ! -e eula.txt ]
