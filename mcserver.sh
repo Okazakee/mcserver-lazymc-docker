99a826c811c8197c704cfba7b9a76a20486e99d3
okazakee <cristiandcbusiness@gmail.com>
2023-01-21 18:33:09 +0100

    another tyopo

--- mcserver.sh
+++ mcserver.sh
@@ -61,10 +61,10 @@ URL=${URL}/versions/${MC_VERSION}
 if [ ${SERVER_BUILD} = latest ]
 then
   # Get the latest build
-  PAPER_BUILD=$(wget -qO - $URL | jq '.builds[-1]')
+  SERVER_BUILD=$(wget -qO - $URL | jq '.builds[-1]')
 fi
-JAR_NAME=${SERVER_PROVIDER}-${MC_VERSION}-${PAPER_BUILD}.jar
-URL=${URL}/builds/${PAPER_BUILD}/downloads/${JAR_NAME}
+JAR_NAME=${SERVER_PROVIDER}-${MC_VERSION}-${SERVER_BUILD}.jar
+URL=${URL}/builds/${SERVER_BUILD}/downloads/${JAR_NAME}
 
 #check if build exists
 status_code=$(curl -s -o /dev/null -w '%{http_code}' ${URL})
